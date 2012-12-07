9a5fa985a92f088843fcbff646ad3b6d17bb080d
Piedone <piedone@pyrocenter.hu>
2012-12-07 15:41:53 +0100

    Adding Ace source, but PHP syntax highlight broke, so continue using CDN version

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -7,6 +7,10 @@ namespace OrchardHUN.Scripting
         public void BuildManifests(ResourceManifestBuilder builder)
         {
             var manifest = builder.Add();
+
+            manifest.DefineScript("AceEditor").SetUrl("ace/min-noconflict/ace.js", "ace/noconflict/ace.js").SetCdn("//d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js");
+            manifest.DefineScript("ScriptingAdmin").SetDependencies("AceEditor", "jQuery");
+            
             manifest.DefineStyle("ScriptingAdmin").SetUrl("orchardhun-scripting-admin.css");
         }
     }
